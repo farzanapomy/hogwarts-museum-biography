79dad39ba09e1cad62344efa9cbf8890f65ad743
farzanapomy <farzanapomy56@gmail.com>
2021-09-26 15:03:21 +0600

    updated

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -6,18 +6,18 @@ const Card = (props) => {
     const { name, img, family, age, price } = props.card
     return (
         <div className='card-section'>
-           
-                <img className='card-img' src={img} alt="" />
-                <h3>{name}</h3>
-                <p>Father :  {family.fatherName}</p>
-                <p>Mother :  {family.motherName}</p>
-                <p>Age : {age}</p>
-                <h4>Price : $ {price}</h4>
-                <button 
-                    onClick={() => props.addToCart(props.card)}
-                    className='cart-button'>
-                    Add To Cart</button>
-          
+
+            <img className='card-img' src={img} alt="" />
+            <h3>{name}</h3>
+            <p>Father :  {family.fatherName}</p>
+            <p>Mother :  {family.motherName}</p>
+            <p>Age : {age}</p>
+            <h4>Price : $ {price}</h4>
+            <button
+                onClick={() => props.addToCart(props.card)}
+                className='cart-button'>
+                Add To Cart</button>
+
         </div>
     );
 };
--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -3,16 +3,23 @@ import './Cart.css'
 const Cart = (props) => {
     const { cart } = props;
     let total = 0;
+    let name = [];
     for (const card of cart) {
         total = total + card.price
+        name = name + '\n' + card.name;
     }
+    // cart.map(singleCart => {
+        
+    // })
 
     return (
         <div className='cart-section'>
             <h3>Name: {cart.name}</h3>
             <h3>Total Added: {props.cart.length}</h3>
             <h4>Total Price : {total}</h4>
-
+            <div>
+                <h5>Name :{name}</h5>
+            </div>
         </div>
     );
 };
